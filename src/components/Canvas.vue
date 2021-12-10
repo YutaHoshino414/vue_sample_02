e4cb28a26bc4b9b4b902f33a7591fd12ca6addb9
USER <y.hoshino25@gmail.com>
2021-12-11 00:05:09 +0900

    edit Canvas/11th/Dec

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,5 +1,5 @@
 <template>
-  <canvas id="myCanvas" width="1400" height="600" class="canvas"></canvas>
+  <canvas id="myCanvas" width="1000" height="800" class="canvas"></canvas>
 </template>
 
 <script>
@@ -13,37 +13,39 @@ export default {
 
     var stage = new Stage("myCanvas");
     console.log(stage)
+    var bmpBG = new Bitmap("https://raw.githubusercontent.com/monsterdive-web-div/vue3x_books/main/sample-app/src/assets/images/hero-bg-home.svg");
+    stage.addChild(bmpBG);
     var bmp1 = new Bitmap("https://raw.githubusercontent.com/monsterdive-web-div/vue3x_books/main/sample-app/src/assets/images/logo-vue.svg");
-    bmp1.scaleX = 0.2;
-    bmp1.scaleY = 0.2;
+    bmp1.scaleX = 0.1;
+    bmp1.scaleY = 0.1;
     stage.addChild(bmp1);
     var bmp2 = new Bitmap("https://raw.githubusercontent.com/YutaHoshino414/vue_sample_02/master/src/assets/logo.svg");
     bmp2.scaleX = 0.2;
     bmp2.scaleY = 0.2;
     stage.addChild(bmp2);
-    
-    bmp1.x = 850;
+
+    bmpBG.y = 200;
+    bmp1.x = 50;
     bmp1.y = 100;
-    bmp2.x = 200;
+    bmp2.x = 800;
     bmp2.y = 100;
     Tween.get(bmp1, {loop: true})
-      .wait(300)
+      .wait(500)
       .to({x: 240, y: 200, scale: 0.7}, 700)
-      .to({x: 400, y: 290, scale: 0.3}, 1200)
-      .to({x: 300, y: 100, scale: 1.2}, 1200)
+      .to({x: 400, y: 290, scale: 0.3}, 1000)
+      .to({x: 300, y: 100, scale: 1.2}, 1000)
       .to({x: 100, y: 50, scale: 0.5}, 700);
 
-      Ticker.addEventListener("tick", handleTick);
+     Ticker.addEventListener("tick", handleTick);
 
-      function handleTick() {
-        // マウス座標を取得する
-        var mx = stage.mouseX;
-        var my = stage.mouseY;
-        // シェイプをマウスに追随させる
-        bmp2.x = mx+50;
-        bmp2.y = my+50;
-      }
-
+    function handleTick() {
+      // マウス座標を取得する
+/*       var mx = stage.mouseX;
+      var my = stage.mouseY;
+      // シェイプをマウスに追随させる
+      bmp2.x = mx+50;
+      bmp2.y = my+50; */
+    } 
     
     update();
     
@@ -52,7 +54,6 @@ export default {
       stage.update();
       requestAnimationFrame(() => update());
     }
-
   }
 }
 </script>
--- src/views/Page4.vue
+++ src/views/Page4.vue
@@ -2,7 +2,7 @@
   <div class="page4">
     <h1 class="mb-5">This is a page 4</h1>
     <Canvas />
-    <br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
   </div>
 </template>
 
